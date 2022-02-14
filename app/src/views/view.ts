0f1888f7a16700148a733129699ca2c77cc936fc
Gabrielle <gabinunes09@gmail.com>
2022-02-14 15:30:55 -0300

    Adicionando decorators de propriedades

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -1,34 +1,26 @@
+import { inspect } from "../decorators/inspect.js";
 import { logarTempoExecucao } from "../decorators/logar-tempo-execucao.js";
 
 abstract class View<T> {
+  protected elemento: HTMLElement;
 
-    protected elemento: HTMLElement;
-    private escape = false;
+  constructor(seletor: string) {
+    //escape com ? faz com que ele seja opcional. Opcionais sempre serão os ultimos parametros
+    const elemento = document.querySelector(seletor);
 
-    constructor(seletor: string, escape?: boolean){ //escape com ? faz com que ele seja opcional. Opcionais sempre serão os ultimos parametros
-        const elemento = document.querySelector(seletor);
-
-        if(elemento){
-            this.elemento = elemento as HTMLElement;
-        } else {
-            throw Error(`Seletor ${seletor} não existe no DOM. Verifique.`)
-        }
-
-        if(escape){
-            this.escape = escape;
-        }
+    if (elemento) {
+      this.elemento = elemento as HTMLElement;
+    } else {
+      throw Error(`Seletor ${seletor} não existe no DOM. Verifique.`);
     }
+  }
 
-    @logarTempoExecucao(true)
-    public update(model: T): void {
-        let template = this.template(model);
-        if(this.escape){
-            template = template.replace(/<script>[\s\S]*?<\/script>/, '');
-        }
-        this.elemento.innerHTML = template;
-    }
+  public update(model: T): void {
+    let template = this.template(model);
+    this.elemento.innerHTML = template;
+  }
 
-    protected abstract template(model: T): string;
+  protected abstract template(model: T): string;
 }
 
-export default View
+export default View;
